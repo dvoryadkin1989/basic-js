0cfdd373141aee4ac84c1e7303648a2bdb12d10e
Sergey Dvaradkin <dvoryadkin1989@gmail.com>
2020-02-16 14:04:37 +0300

    feat: implement count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,15 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+    if (!backyard) {
+        return 0;
+    }
+    let result = 0;
+    for (let i = 0; i < backyard.length; i++) {
+        for (let j = 0; j < backyard[i].length; j++) {
+            if (backyard[i][j] === "^^") {
+                result++;
+            }
+        }
+    }
+    return result;
 };
+
