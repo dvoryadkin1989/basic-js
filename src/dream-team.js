8960faced3768125947555ddef29ea6731869032
Siarhei Dvaradkin <s.dvaradkin@godeltech.com>
2020-02-18 15:57:38 +0300

    feat: implement dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,14 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  return members
+      .filter(m => typeof m === 'string')
+      .filter(m => m.length > 0)
+      .map(m => m.trim())
+      .map(m => m.charAt(0).toUpperCase())
+      .sort()
+      .join("");
+}
+
+module.exports =  createDreamTeam;
