b54ad19795e1a6864bf5c508bdea323cd523ed70
Sergey Dvaradkin <dvoryadkin1989@gmail.com>
2020-02-19 03:33:41 +0300

    feat: implement carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,22 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+function dateSample(sampleActivityStr) {
+  if (!sampleActivityStr || typeof sampleActivityStr !== "string") {
+    return false;
+  }
+  const sampleActivity = parseFloat(sampleActivityStr);
+  if (!sampleActivity) {
+    return false;
+  }
+  if (sampleActivity <= 0 || sampleActivity > 15) {
+    return false;
+  }
+
+  return Math.ceil(-Math.log(sampleActivity / MODERN_ACTIVITY) * HALF_LIFE_PERIOD / 0.693);
+}
+
+
+// assert.equal(dateSample('11.3231.3213124'), 2326);
+console.log(dateSample("11.3231.3213124"));
+module.exports = dateSample;
