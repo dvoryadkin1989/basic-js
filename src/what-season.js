415fee5de5a56319610dafee027220933c92df7d
Sergey Dvaradkin <dvoryadkin1989@gmail.com>
2020-02-19 20:53:20 +0300

    feat: implement what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,21 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+function getSeason(date) {
+  if (date === undefined) {
+    return "Unable to determine the time of year!";
+  }
+  if (Object.prototype.toString.call(date) !== "[object Date]") {
+    throw new Error("Invalid input param " + date);
+  }
+  const month = date.getMonth();
+  if (month < 2 || month === 11) {
+    return "winter"
+  }
+  if (month < 5) {
+    return "spring"
+  }
+  if (month < 8) {
+    return "summer"
+  }
+  return "fall"
+}
+
+module.exports = getSeason;
